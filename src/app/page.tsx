a909e51f466b083a5d009c309f0933a1a5559ba1
hermes7144 <kichancho@gmail.com>
2023-07-04 17:33:24 +0900

    ver3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,9 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <>
-      <h1>홈페이지다 2</h1>
+      <h1>홈페이지다 3</h1>
       <Counter />
       <Image src='https://images.unsplash.com/photo-1441986300917-64674bd600d8' alt='shop' width={400} height={400} />
     </>
   );
 }
-
